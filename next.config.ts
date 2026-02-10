07b401fc2bad2f879255023271d7223a3f5fcf00
usama-omb-dev <usama@onlinemarketingbakery.nl>
2026-02-11 04:33:17 +0500

    init 2

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
   /* config options here */
 };
 
